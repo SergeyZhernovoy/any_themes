a338da644cd08a24a3fb358317685a81e94070e5
SergeyZhernovoy <s_jernov@mail.ru>
2017-06-17 21:18:22 +0300

    добавил пример по паттерну конструктор и по рефлексии

---
+++ PartVII/lesson1/src/main/java/ru/zhernovoy/lesson3/Entity.java
@@ -0,0 +1,47 @@
+package ru.zhernovoy.lesson3;/**
+ * Created by Admin on 17.06.2017.
+ */
+
+/**
+ * Реализован шаблон конструктор, для избавления от телескопических конструкторов
+ */
+
+public class Entity {
+
+    protected Long id;
+    protected String description;
+
+    private Entity(Builder builder){
+            this.id = builder.id;
+            this.description = builder.description;
+    }
+
+    public static class Builder {
+
+        Long id = Long.valueOf(0);
+        String description = "";
+
+        public Builder setIdEntity(Long id){
+            this.id = id;
+            return this;
+        }
+
+        public Builder setDescriptionEntity(String description){
+            this.description = description;
+            return this;
+        }
+
+        public Entity build(){
+            Entity entity = new Entity(this);
+            return entity;
+        }
+    }
+
+    public static void main(String[] args) {
+        Entity newEntity = new Entity.Builder()
+                                     .setIdEntity(1L)
+                                     .setDescriptionEntity("Test")
+                                     .build();
+    }
+
+}
