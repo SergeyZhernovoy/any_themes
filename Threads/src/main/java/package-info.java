9f170a30d328f32e816db62558ec68fe153114ba
Sergey Zhernovoy <s_jernov@mail.ru>
2017-09-04 18:26:57 +0300

    выбрал книги по многопоточности

---
+++ Threads/src/main/java/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author Sergey Zhernovoy
+ * create on 04.09.2017.
+
+Gonzalez J.F. - Mastering Concurrency Programming with Java 8 - 2016.pdf
+Java Threads and the Concurrency Utilities.pdf  - читаю эту , текущая страница 14
+
+ */
+
