b3a8781829d3a43a2992f47ab0111dfe3805c9f6
SergeyZhernovoy <s_jernov@mail.ru>
2017-09-07 00:46:35 +0300

    update от 07/09/2017

--- Threads/src/main/java/package-info.java
+++ Threads/src/main/java/package-info.java
@@ -3,7 +3,7 @@
  * create on 04.09.2017.
 
 Gonzalez J.F. - Mastering Concurrency Programming with Java 8 - 2016.pdf
-Java Threads and the Concurrency Utilities.pdf  - читаю эту , текущая страница 30
+Java Threads and the Concurrency Utilities.pdf  - читаю эту , текущая страница 33
 
  */
 
