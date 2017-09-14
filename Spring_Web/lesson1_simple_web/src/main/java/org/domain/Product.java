aa149a29c9f2ebde40c26919d830165136c657e3
Sergey Zhernovoy <s_jernov@mail.ru>
2017-09-14 23:54:12 +0300

    добавил в хелло проект веб уровни МВС

---
+++ Spring_Web/lesson1_simple_web/src/main/java/org/domain/Product.java
@@ -0,0 +1,112 @@
+package org.domain;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Sergey Zhernovoy
+ * create on 14.09.2017.
+ */
+
+public class Product {
+    private String productId;
+    private String name;
+    private BigDecimal unitPrice;
+    private String manufactured;
+    private String description;
+    private String category;
+    private long unitsInStock;
+    private long unitsInOrder;
+    private boolean discounted;
+    private String condition;
+
+    public Product(String productId, String name, BigDecimal unitPrice) {
+        this.productId = productId;
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+    public Product() {
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getManufactured() {
+        return manufactured;
+    }
+
+    public void setManufactured(String manufactured) {
+        this.manufactured = manufactured;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public long getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(long unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public long getUnitsInOrder() {
+        return unitsInOrder;
+    }
+
+    public void setUnitsInOrder(long unitsInOrder) {
+        this.unitsInOrder = unitsInOrder;
+    }
+
+    public boolean isDiscounted() {
+        return discounted;
+    }
+
+    public void setDiscounted(boolean discounted) {
+        this.discounted = discounted;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+}
+
+    
