7b612f985375225d711f40dc86bb17f6d330aa8f
Sergey Zhernovoy <s_jernov@mail.ru>
2017-09-21 18:20:58 +0300

    книги по лямбдам , упражнение по IO

--- Io_Nio_Nio2/src/main/java/lesson1/HelloWorld_IO.java
+++ Io_Nio_Nio2/src/main/java/lesson1/HelloWorld_IO.java
@@ -1,9 +1,45 @@
-package lesson1;/**
+package lesson1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+/**
  * @author Sergey Zhernovoy
  * create on 21.09.2017.
  */
 
 public class HelloWorld_IO {
+    public static void main(String[] args) throws IOException {
+
+        if(args.length != 1){
+            System.err.println("usage: java PathInfo path");
+            System.exit(1);
+        } else {
+            File file  = new File(args[0]);
+
+            System.out.println(" ---------- PATH---------");
+
+            System.out.println("Absolute path: " + file.getAbsolutePath());
+            System.out.println("Canoncial path: " + file.getCanonicalPath());
+            System.out.println("Name = " + file.getName());
+            System.out.println("Parent = " + file.getParent());
+            System.out.println("Path = " + file.getPath());
+            System.out.println("Is absolute =  " + file.isAbsolute());
+
+            System.out.println("-----------PROPERTY FILE/DIRECTORY----------");
+
+            System.out.println("About = " + file + ":");
+            System.out.println("Exists " + file.exists());
+            System.out.println("Is directory = " + file.isDirectory());
+            System.out.println("Is file = " + file.isFile());
+            System.out.println("Is hidden = " + file.isHidden());
+            System.out.println("Last modified =  " + new Date(file.lastModified()));
+            System.out.println("Length =  " + file.length());
+        }
+
+
+    }
 }
 
     
---
+++ Io_Nio_Nio2/src/main/java/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * @author Sergey Zhernovoy
+ * create on 21.09.2017.
+ */
+
+/**
+
+читаю
+
+ Нимейр П., Леук Д. - Программирование на Java (2014).pdf глава 12 стр 561
+
+ Java I_O, NIO and NIO.2.pdf стр 27
+
+
+ */
