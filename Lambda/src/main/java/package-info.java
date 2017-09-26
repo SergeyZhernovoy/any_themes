fe55c753d3f2a73c5c2a018eda6616f267e59024
Sergey Zhernovoy <s_jernov@mail.ru>
2017-09-27 00:37:14 +0300

    лямбды

--- Lambda/src/main/java/package-info.java
+++ Lambda/src/main/java/package-info.java
@@ -4,18 +4,24 @@
  */
 
 /**
+ *
+ *  Уорбэртон Р. - Лямбда-выражения в Java 8 (2014).pdf
+ *
+ *  JavaSE8.Вводный.курс.Хорстманн.2014.pdf текущий 37
+ *
+ *
+ * Java Closures and Lambda.pdf
+ *
  Josh Juneau - Java 8 Recipes (The Expert's Voice in Java) - 2014.pdf часть 6
 
 
- JavaSE8.Вводный.курс.Хорстманн.2014.pdf текущий 33
- Уорбэртон Р. - Лямбда-выражения в Java 8 (2014).pdf
+
+
  Хорстман К. - Java SE 8 Базовый курс (2015).djvu  глава 3 и глава 8
- Шилдт Г. - Java 8. Полное руководство (2015).pdf глава 15 и глава 20
- Java 8. Руководство для начинающих. 6-е издание (2015).pdf глава 14
  Mastering Lambdas- Java Programming in a Multicore World.pdf
  Java Lambdas and Parallel Streams.pdf
  Alan Mycroft, Mario Fusco, Raoul-Gabriel Urma Java 8 in Action (2015).pdf
- Java Closures and Lambda.pdf
+
 
 
 
