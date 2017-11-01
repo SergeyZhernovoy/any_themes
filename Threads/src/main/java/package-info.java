99f2b764c1207dbd02072731453c1b4ba03bee0b
Sergey Zhernovoy <s_jernov@mail.ru>
2017-11-02 00:45:38 +0300

    java in practice

--- Threads/src/main/java/package-info.java
+++ Threads/src/main/java/package-info.java
@@ -6,5 +6,9 @@ Gonzalez J.F. - Mastering Concurrency Programming with Java 8 - 2016.pdf
 Java Threads and the Concurrency Utilities.pdf  - читаю эту , текущая страница 58 (69)
 Хорстманн смотрю тему по многопоточности с 745 страницы
 
+a class is threadͲ safe when it continues to behave correctly when accessed from
+multiplethreads.
+
+
  */
 
