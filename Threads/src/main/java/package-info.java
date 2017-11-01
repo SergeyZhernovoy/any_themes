7feda4cdda0014c9dc6118273fdbc6354a381e71
Sergey Zhernovoy <szhernov@gmail.com>
2017-11-01 18:56:39 +0300

    составил новый список по многопоточности

--- Threads/src/main/java/package-info.java
+++ Threads/src/main/java/package-info.java
@@ -2,9 +2,25 @@
  * @author Sergey Zhernovoy
  * create on 04.09.2017.
 
-Gonzalez J.F. - Mastering Concurrency Programming with Java 8 - 2016.pdf
-Java Threads and the Concurrency Utilities.pdf  - читаю эту , текущая страница 58 (69)
-Хорстманн смотрю тему по многопоточности с 745 страницы
+Goetz B. - Java Concurrency in Practice - 2006.pdf (англ)  18
+Java Threads, 2Ed (O'Reilly).pdf (англ)
+Java Threads and the Concurrency Utilities.pdf (англ)
+j-jtp10264-pdf.pdf - статья
+Gonzalez J.F. - Mastering Concurrency Programming with Java 8 - 2016.pdf (англ)
+Java Programming.pdf (англ)
+Нимейр П., Леук Д. - Программирование на Java (2014).pdf
+Шилдт Г. - Java 8. Полное руководство (2015).pdf
+Kishori Sharan - Beginning Java 8 Language Features (The Expert's Voice in Java) - 2014.pdf (англ)
+Язык программирования Java SE 8. Подробное описание.djvu
+Хорстманн К. - Java. Библиотека профессионала. Том 1. Основы - 2016.djvu
+Блох - Java Эффективное программирование.djvu
+Брюс Эккель - Философия Java - 2015.djvu
+
+
+  - Основные принципы многопоточности
+  - утилиты многопоточности
+
+
 
  */
 
