62032f481f9ff469e4d16fbc9d44b4a36afe0163
SergeyZhernovoy <s_jernov@mail.ru>
2017-11-05 00:46:23 +0300

    обновление списка литературы +  обычная многопоточность

--- Threads/src/main/java/package-info.java
+++ Threads/src/main/java/package-info.java
@@ -17,11 +17,10 @@ Java Threads and the Concurrency Utilities.pdf (англ)
 j-jtp10264-pdf.pdf - статья
 Gonzalez J.F. - Mastering Concurrency Programming with Java 8 - 2016.pdf (англ)
 Java Programming.pdf (англ)
-Нимейр П., Леук Д. - Программирование на Java (2014).pdf
-Шилдт Г. - Java 8. Полное руководство (2015).pdf          ------------- V 298
 Kishori Sharan - Beginning Java 8 Language Features (The Expert's Voice in Java) - 2014.pdf (англ)
-Язык программирования Java SE 8. Подробное описание.djvu
-Хорстманн К. - Java. Библиотека профессионала. Том 1. Основы - 2016.djvu
+
+Нимейр П., Леук Д. - Программирование на Java (2014).pdf  -------------- X 406
+Шилдт Г. - Java 8. Полное руководство (2015).pdf          ------------- Х 315 (тема по основным моментам многопоточности закончена, осталась только тема утили глава 28) HR
 Блох - Java Эффективное программирование.djvu
 Брюс Эккель - Философия Java - 2015.djvu
 
