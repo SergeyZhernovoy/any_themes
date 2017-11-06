95709d34a0609f7bf284c04b968a7b0ccdf09106
SergeyZhernovoy <s_jernov@mail.ru>
2017-11-06 21:34:05 +0300

    обновление списка литературы , приступил к изучению утилит многопоточности

--- Threads/src/main/java/package-info.java
+++ Threads/src/main/java/package-info.java
@@ -9,15 +9,13 @@ multiple threads.
 
 3) Не имеющие состояния объекты являются потокобезопасными
 
+4) Необходимо избегать осуществлять долговременные операции с использованием синхронизированных методов , таких как сетевое взаимодействие или использование подсистемы ввода / вывода
 
-
-Goetz B. - Java Concurrency in Practice - 2006.pdf (англ) ------------- 34 X
-Java Threads, 2Ed (O'Reilly).pdf (англ)
+Goetz B. - Java Concurrency in Practice - 2006.pdf (англ) ------------- 39 Х
 Java Threads and the Concurrency Utilities.pdf (англ)
 j-jtp10264-pdf.pdf - статья
 Gonzalez J.F. - Mastering Concurrency Programming with Java 8 - 2016.pdf (англ)
 Java Programming.pdf (англ)
-Kishori Sharan - Beginning Java 8 Language Features (The Expert's Voice in Java) - 2014.pdf (англ)
 
 Нимейр П., Леук Д. - Программирование на Java (2014).pdf  -------------- X 406
 Шилдт Г. - Java 8. Полное руководство (2015).pdf          ------------- Х 315 (тема по основным моментам многопоточности закончена, осталась только тема утили глава 28) HR
