41de9e8dd3f55992109d50833262a90f680aa7ab
SergeyZhernovoy <s_jernov@mail.ru>
2017-11-12 21:17:14 +0300

    проект myBatis

---
+++ myBatis/src/main/java/lesson1_ibatis/HelloWorld.java
@@ -0,0 +1,13 @@
+package lesson1_ibatis;/**
+ * @author Sergey Zhernovoy
+ * create on 12.11.2017.
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+}
+
+    
