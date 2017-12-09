4a4408bcb6774ee88714cf165b89bce150c0320e
Sergey Zhernovoy <s_jernov@mail.ru>
2017-12-09 11:25:37 +0300

    concurrent utility

---
+++ Threads/src/main/java/lesson5_executors/client_server/util/Command.java
@@ -0,0 +1,18 @@
+package lesson5_executors.client_server.util;/**
+ * @author Sergey Zhernovoy
+ * create on 09.12.2017.
+ */
+
+public abstract class Command {
+
+    protected String[] command;
+
+    public Command(String[] command){
+        this.command = command;
+    }
+
+    public abstract String execute();
+
+}
+
+    
