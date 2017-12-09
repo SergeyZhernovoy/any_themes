951ff6d063f2dfbc23b9314629847434ca004aa9
Sergey Zhernovoy <s_jernov@mail.ru>
2017-12-10 00:50:51 +0300

    concurrent utility day 2

--- Threads/src/main/java/package-info.java
+++ Threads/src/main/java/package-info.java
@@ -2,6 +2,9 @@
  * @author Sergey Zhernovoy
  * create on 04.09.2017.
 
+Gonzalez J.F. - Mastering Concurrency Programming with Java 8 - 2016.pdf (англ) V 73
+
+
 1) a class is thread safe when it continues to behave correctly when accessed from
 multiple threads.
 
@@ -14,7 +17,7 @@ multiple threads.
 Goetz B. - Java Concurrency in Practice - 2006.pdf (англ) ------------- 39 Х
 Java Threads and the Concurrency Utilities.pdf (англ)
 j-jtp10264-pdf.pdf - статья
-Gonzalez J.F. - Mastering Concurrency Programming with Java 8 - 2016.pdf (англ) V 43(68)
+
 Java Programming.pdf (англ)
 
 Нимейр П., Леук Д. - Программирование на Java (2014).pdf  -------------- X 406
