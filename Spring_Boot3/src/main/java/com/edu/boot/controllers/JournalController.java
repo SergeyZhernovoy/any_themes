0d18c2156fc102341961e45b402700a387e08868
Sergey Zhernovoy <szhernov@gmail.com>
2018-01-18 18:37:45 +0300

    spring boot3 journal

--- Spring_Boot3/src/main/java/com/edu/boot/controllers/JournalController.java
+++ Spring_Boot3/src/main/java/com/edu/boot/controllers/JournalController.java
@@ -1,11 +1,15 @@
 package com.edu.boot.controllers;
 
+import com.edu.boot.domain.Journal;
 import com.edu.boot.repository.JournalRepositroy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class JournalController {
@@ -17,4 +21,11 @@ public class JournalController {
         model.addAttribute("journal",repo.findAll());
         return "index";
     }
+
+    @RequestMapping(value = "/journal",produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @ResponseBody
+    public List<Journal> getJournal(){
+        return repo.findAll();
+    }
+
 }
