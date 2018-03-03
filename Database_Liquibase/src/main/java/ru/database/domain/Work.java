fdf7faac399094abe0f37214211de8b28cf9eb5f
Sergey Zhernovoy <szhernov@gmail.com>
2018-03-03 08:06:54 +0300

    изучаем по бд , день 3 миграция работает

--- Database_Liquibase/src/main/java/ru/database/domain/Work.java
+++ Database_Liquibase/src/main/java/ru/database/domain/Work.java
@@ -1,4 +1,13 @@
 package ru.database.domain;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class Work {
+	private Long id;
+	private String description;
+	private LocalDate moment;
+
 }
