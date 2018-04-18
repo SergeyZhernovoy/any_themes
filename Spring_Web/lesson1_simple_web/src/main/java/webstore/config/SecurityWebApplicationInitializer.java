f955fa7f11b244c349daaec01b76996223941945
Sergey Zhernovoy <szhernovoy@gmail.com>
2018-04-18 18:05:00 +0300

    корректировка по секьюрити 1

--- Spring_Web/lesson1_simple_web/src/main/java/webstore/config/SecurityWebApplicationInitializer.java
+++ Spring_Web/lesson1_simple_web/src/main/java/webstore/config/SecurityWebApplicationInitializer.java
@@ -4,4 +4,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+	public SecurityWebApplicationInitializer() {
+		super(SecurityConfig.class);
+	}
 }
