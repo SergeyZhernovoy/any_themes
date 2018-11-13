808291d149e51c96838f048d790a4495ad037040
= <szhernov@gmail.com>
2018-11-13 18:31:06 +0300

    update book's page

--- ejb/jpa/src/main/java/package-info.java
+++ ejb/jpa/src/main/java/package-info.java
@@ -1,6 +1,6 @@
 /**
 
- Pro JPA 2 in Java EE 8, 3rd Edition.pdf
+ Pro JPA 2 in Java EE 8, 3rd Edition.pdf - 59
  Keith M., Schincariol M. - Pro JPA 2, 2nd Edition (The Expert's Voice in Java) - 2013.pdf
 
 
