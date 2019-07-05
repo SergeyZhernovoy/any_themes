a7667f44440b113ebe79f9adb655151b91b6b1f1
Sergey Zhernovoy <szhernov@gmail.com>
2019-07-05 17:26:46 +0300

    refactoring treads

--- Threads/src/main/java/lesson5_executors/client_server/util/CacheExecute.java
+++
@@ -1,9 +0,0 @@
-package lesson5_executors.client_server.util;
-
-public interface CacheExecute {
-
-    boolean isCacheable();
-
-    void setCacheable(boolean cacheable);
-
-}
--- Threads/src/main/java/lesson5_executors/client_server/util/TaskExecute.java
+++
@@ -1,5 +0,0 @@
-package lesson5_executors.client_server.util;
-
-public interface TaskExecute {
-    String execute();
-}
